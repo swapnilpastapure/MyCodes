c0c9295629655e45b4f4305ed6c97eb98ed9915d
Swapnil <swapnilbalajipastapure@gmail.com>
2026-06-01 23:36:37 +0530

    adding one

---
+++ Java/LeetCode/RomanToInteger.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+
+    public int romanToInt(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int current = map.get(s.charAt(i));
+
+            if (i < s.length() - 1 &&
+                    current < map.get(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        RomanToInteger solution = new RomanToInteger();
+
+        System.out.println(solution.romanToInt("III"));      // 3
+        System.out.println(solution.romanToInt("LVIII"));    // 58
+        System.out.println(solution.romanToInt("MCMXCIV"));  // 1994
+    }
+}
